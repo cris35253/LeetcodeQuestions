c49b1f9f872717ae6b818f5ac4eeb90f9c37ff1d
cristian <c21347906@mytudublin.ie>
2025-11-21 18:55:16 +0000

    My implementation of Leetcode problem 28. Python, Java and C# version

---
+++ Top150Interview/C#/28_Index_First_Occurence_String.cs
@@ -0,0 +1,21 @@
+class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (needle.Length == 0){
+            return 0;
+        }
+
+        for (int i = 0; i <= haystack.Length - needle.Length; i++) {
+            for (int j = 0; j < needle.Length; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+
+                if (j == needle.Length - 1) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}

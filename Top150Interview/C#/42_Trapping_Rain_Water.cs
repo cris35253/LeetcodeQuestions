3f6c8c651a46748d9ed36339ca41c6e2a99ad978
cristian <c21347906@mytudublin.ie>
2025-11-21 19:24:04 +0000

    My implementation of Leetcode problem 42. Python, Java and C# version

---
+++ Top150Interview/C#/42_Trapping_Rain_Water.cs
@@ -0,0 +1,29 @@
+class Solution {
+    public int Trap(int[] height) {
+        if (height == null || height.Length == 0) {
+            return 0;
+        }
+
+        int l = 0;
+        int r = height.Length - 1;
+        int leftMax = height[l];
+        int rightMax = height[r];
+
+        int res = 0;
+
+        while (l < r) {
+            if (leftMax < rightMax){
+                l += 1;
+                leftMax = Max.Max(leftMax, height[l]);
+                res += leftMax - height[l];
+            }
+            else {
+                r -= 1;
+                rightMax = Math.Max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+
+        return res;
+    }
+}
